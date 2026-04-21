1b41bf6ebb7b530d5df7bcf6337266759b64595b
damed2192-svg <damed2192@gmail.com>
2026-04-21 11:14:15 +0100

    Create rules.py

---
+++ rules.py
@@ -0,0 +1,6 @@
+RULES = [
+    {"keyword": "hello", "response": "Xin chào 👋"},
+    {"keyword": "hi", "response": "Chào bạn!"},
+    {"keyword": "bye", "response": "Tạm biệt nhé!"},
+    {"keyword": "name", "response": "Tôi là chatbot API 🤖"},
+]
